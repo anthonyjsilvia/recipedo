bdea14a14f7b8f30a8edad45604417da91931d6a
Anthony Silvia <61393689+anthonyjsilvia@users.noreply.github.com>
2023-03-04 13:12:37 -0500

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -4,14 +4,18 @@
     window.location.href = "https://nodeda.com/recipedo/download";
   }
 
-  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+  if (/iPhone|iPod/.test(userAgent) && !window.MSStream) {
     window.location.href = "https://nodeda.com/recipedo/download";
   }
   var downloadLink = document.querySelector('.header-nav ul li a');
-  
+
   if (/Macintosh/.test(userAgent)) {
     if (/Intel/.test(userAgent) || /Apple/.test(userAgent)) {
       downloadLink.innerHTML = 'Download for Mac';
       downloadLink.href = 'https://nodeda.com/recipedo';
     }
+  }
+  else if (/iPad/.test(userAgent)) {
+    downloadLink.innerHTML = 'Download for iPad';
+    downloadLink.href = 'https://apps.apple.com/us/app/recipedo-gallery/id1564537067';
   }
