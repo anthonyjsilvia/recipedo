0bfd1ee7246ab2eff531ce51d89ffb767747eb43
Anthony Silvia <61393689+anthonyjsilvia@users.noreply.github.com>
2023-03-04 13:10:51 -0500

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -7,3 +7,11 @@
   if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
     window.location.href = "https://nodeda.com/recipedo/download";
   }
+  var downloadLink = document.querySelector('.header-nav ul li a');
+  
+  if (/Macintosh/.test(userAgent)) {
+    if (/Intel/.test(userAgent) || /Apple/.test(userAgent)) {
+      downloadLink.innerHTML = 'Download for Mac';
+      downloadLink.href = 'https://nodeda.com/recipedo';
+    }
+  }
